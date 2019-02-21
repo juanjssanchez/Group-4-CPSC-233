f7f7921e002a6a1a29d8a937551fd7d1ae76ac1e
FysalBeau <47364533+FysalBeau@users.noreply.github.com>
2019-02-21 11:02:38 -0700

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -1,4 +1,18 @@
-public enum Direction{
-    
+public enum Direction {
+    // enum constants with parameters dx and dy directions 
+    NORTH(0,1),
+    EAST(1,0),
+    SOUTH(0,-1),
+    WEST(-1,0);
+
+
+
+    private Direction (int dx, int dy){     // constructor
+        this.dx = dx;
+        this.dy = dy;
+    }
+    private final int dx;    // instance variables
+    private final int dy;
+
 
 }
