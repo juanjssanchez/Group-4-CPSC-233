7f8076cd39df5bdab0b9e647a70ca5a4d63ff2da
juanjssanchez <47364524+juanjssanchez@users.noreply.github.com>
2019-02-21 09:33:03 -0700

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,4 +1,9 @@
 class Location {
+
+/*Location is the class that indicates the location on the map.  
+-Contains x and y coordinates of the grid
+-methods for getting x and y coords*/
+    
     private int xcoord;
     private int ycoord;
 
