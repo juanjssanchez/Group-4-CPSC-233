c9567cf84a12a206052c8aabcbd6315fbc9ef9f0
shamimkhalili <47364526+shamimkhalili@users.noreply.github.com>
2019-02-21 10:51:41 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,5 @@
 public class Player extends Person{
-    private int health;
     private ArrayLlist<Collecible> items;
-    private Location location;
  
     int potion = 30;
     int maxPlayerDamage = 30;
@@ -12,9 +10,6 @@ public class Player extends Person{
         return;
     }
 
-    public int getHealth(){
-        return health;
-    }
     public void usePotion(){
         health += potion;
     }
