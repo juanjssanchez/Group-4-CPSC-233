cad4155114ac5dfecc096dbf217ad81f790ad8cf
shamimkhalili <47364526+shamimkhalili@users.noreply.github.com>
2019-02-21 09:27:46 -0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,27 @@
+public class Person {
+
+/*Enemy is a class that represents enemies the player/avatar must battle when encountered on the map.  
+-The enemy should have a health
+
+-and a method that indicates how to attack during each turn/round.
+
+-You may also need a boolean method that indicates if the Enemy is alive or dead.*/
+
+
+	private int health;
+	private Location location;
+
+
+	public attackType getMove(){
+		return ;
+	}
+
+	public int decreaseHealth(){
+		
+		return 
+	}
+
+	public boolean aliveOrDead(){
+		return false;
+	}
+}
