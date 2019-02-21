da96f1558f53321edd8e73780197bd0eeb2eb18c
FysalBeau <47364533+FysalBeau@users.noreply.github.com>
2019-02-21 10:09:49 -0700

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,3 +1,23 @@
 public class Map{
-    
+
+    private char [][] grid;
+
+
+    public char getElement(Location location){
+
+        return '\u0000';
+    }
+
+    public boolean isValidMove(Location currentLocation){
+
+        return true;
+    }
+
+    public void move(Location currentLocation, Direction direction){
+
+    }
+
+
+
 }
+
