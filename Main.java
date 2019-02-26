dbcd9819b7eed44ab23ceef0115fbc5eb022239a
juanjssanchez <47364524+juanjssanchez@users.noreply.github.com>
2019-02-25 21:01:59 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,207 +2,6 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
 
-
-/* *** Extremely rough draft of how the text based game might work ***
-     -this is just a starting point to work from
-     -still need to incorporate the other classes to work with this 
-     
-     Use WASD and press ENTER to move
-     Player is represented by X and enemies by M
-     Move near enemy to trigger combat stage
-     Theres only one map right now
-     Note: although the enemies always start in same spot, their stats are randomized 
-     */
-
-
-public class Main extends Map{
-    public static void main(String[] args){
-
-        Scanner in = new Scanner(System.in);
-        Random rand = new Random();
-
-        //Enemy stats
-        String[] enemies = {"Monster", "Scary Monster"};
-        int maxEnemyHealth = 75;
-        int enemyAttackDamage = 25;
-
-        int numCaptives = 0;
-
-        //Player stats
-        int health = 100;
-        int attackDamage = 50;
-        int numHealthPotions = 3;
-        int healthPotionHealAmount = 30;
-        int healthPotionDropChance = 50; //percentage
-
-        boolean running = true;
-        boolean walking = true;
-
-        System.out.println("__          __  _ \n" +
-                "\\ \\        / / | |\n" +
-                " \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___ \n" +
-                "  \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ \n" +
-                "   \\  /\\  /  __/ | (_| (_) | | | | | |  __/\n" +
-                "    \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|");
-
-
-        //two dimensional array(map)
-        char[][] arr = {
-                {'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'},
-                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
-                {'H', ' ', 'M', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'M', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
-                {'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'},
-        };
-
-
-        //starting the game
-        while(true) {
-            System.out.println("\t\nPress ENTER to start game");
-            String input = in.nextLine();
-            if (input.equals("")) {
-                break;
-            } else {
-                System.out.println("Invalid command");
-            }
-        }
-
-        //Main game begins
-        GAME:
-        while(running) {
-            System.out.println("------------------------------------------\n");
-
-            //movement around screen
-
-            while(walking){
-                //updates screen
-                for (int i = 0; i < arr.length; i++) {
-                    for (int j = 0; (arr[i] != null && j < arr[i].length); j++)
-                        System.out.print(arr[i][j] + " ");
-                    System.out.println();
-                }
-                System.out.println("Number of captives: " + numCaptives);
-
-                //moves using WASD and ENTER
-                String input = in.nextLine();
-                moveloop:
-                for (int i = 0; i < 9; i++)
-                    for (int j = 0; j < 16; j++)
-                        if (arr[i][j] == 'X') {
-                            if (input.equals("w")) {
-                                if (arr[i - 1][j] == ' ') {
-                                    arr[i][j] = ' ';
-                                    arr[i - 1][j] = 'X';
-                                    break;
-                                }
-                            }
-                            else if (input.equals("a")) {
-                                if (arr[i][j - 1] == ' ') {
-                                    arr[i][j] = ' ';
-                                    arr[i][j - 1] = 'X';
-                                    break;
-                                }
-                            }
-                            else if (input.equals("d")) {
-                                if (arr[i][j + 1] == ' ') {
-                                    arr[i][j] = ' ';
-                                    arr[i][j + 1] = 'X';
-                                    break;
-                                }
-                            }
-                            else if (input.equals("s")) {
-                                if (arr[i + 1][j] == ' ') {
-                                    arr[i][j] = ' ';
-                                    arr[i + 1][j] = 'X';
-                                    System.out.println('w');
-                                    break moveloop;
-                                }
-                            }
-                        }
-                //enemy nearby
-                for (int i = 0; i < 9; i++)
-                    for (int j = 0; j < 16; j++)
-                        if (arr[i][j] == 'X'){
-                            if (arr[i+1][j] == 'M' || arr[i-1][j] == 'M' || arr[i][j+1] == 'M' || arr[i][j-1] == 'M'){
-                                walking = false;
-                            }
-                        }
-            }
-            walking = true;
-
-            //randomizes enemy stats
-            int enemyHealth = rand.nextInt(maxEnemyHealth);
-            String enemy = enemies[rand.nextInt(enemies.length)];
-            System.out.println("\n\t# OH NO, there's a " + enemy + "!! #\n");
-
-            // while enemy alive, choose what to do
-            while(enemyHealth > 0){
-                System.out.println("\tYour HP: " + health);
-                System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
-                System.out.println("\n\tWhat are you going to do?");
-                System.out.println("\t1. Attack");
-                System.out.println("\t2. Drink potion");
-                System.out.println("\t3. Run to next room");
-
-                //attack option
-                String input = in.nextLine();
-                if(input.equals("1")){
-                    int damageDealt = rand.nextInt(attackDamage);
-                    int damageTaken = rand.nextInt(enemyAttackDamage);
-
-                    enemyHealth -= damageDealt;
-                    health -= damageTaken;
-
-                    System.out.println("\t> You strike the " + enemy + " for " + damageDealt + " damage.");
-                    System.out.println("\t> You recieve " + damageTaken + " in retaliation!");
-
-                    if (health < 1){
-                        System.out.println("\t> You have taken too much damage");
-                        break;
-
-                    }
-                }
-
-                //healing option
-                else if(input.equals("2")){
-                    if (numHealthPotions > 0){
-                        health += healthPotionHealAmount;
-                        numHealthPotions--;
-                        System.out.println("\t> You drink a potion, healing for " +healthPotionHealAmount + "."
-                                            + "\n\t> You now have " + health + " HP."
-                                            + "\n\t> You have " + numHealthPotions + " health potions left.\n");
-                    }
-                    else {
-                        System.out.println("\t> You have no health potions left");
-                    }
-
-                }
-
-                //scaredy cat option
-                else if(input.equals("3")){
-                    System.out.println("\tYou have run away from the " + enemy + "!");
-                    continue GAME;
-                }
-
-                //wrong input
-                else{
-                    System.out.println("\tInvalid command");
-
-                }
-            }
-
-            //game ends if you run out of health
-            if (health < 1){
-                System.out.println("You limp out of the dungeon, weak from battle");
-                break;
-import java.util.Scanner;
-import java.util.Random;
-import java.util.Arrays;
-
 /* *** Extremely rough draft of how the text based game might work ***
      -this is just a starting point to work from
      -still need to incorporate the other classes to work with this
@@ -244,7 +43,7 @@ public class MainBackUp extends Map{
                 "  \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ \n" +
                 "   \\  /\\  /  __/ | (_| (_) | | | | | |  __/\n" +
                 "    \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|");
-        
+
         //starting the game
         while(true) {
             System.out.println("\t\nPress ENTER to start game");
@@ -259,8 +58,8 @@ public class MainBackUp extends Map{
         //creates map object
         Map map1 = new Map();
 
-        
-        
+
+
         //Main game begins
         GAME:
         while(running) {
@@ -398,3 +197,4 @@ public class MainBackUp extends Map{
 
 }
 
+ 
