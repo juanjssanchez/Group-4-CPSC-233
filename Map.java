ee86016388ca482dc4bdfe9cac75bf6173ae7552
juanjssanchez <47364524+juanjssanchez@users.noreply.github.com>
2019-02-25 20:52:59 -0700

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,8 +1,32 @@
+import java.util.Arrays;
+
 public class Map{
 
-    private char [][] grid;
+    private char[][] arr;
+
+    Map(){
+        arr = new char[][]{
+                {'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'},
+                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
+                {'H', ' ', 'M', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'M', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'H'},
+                {'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'},
+        };
+    }
+
+    public char[][] getMap(){
+        return arr;
+    }
 
 
+
+
+
+    //idk about these
     public char getElement(Location location){
 
         return '\u0000';
@@ -13,11 +37,73 @@ public class Map{
         return true;
     }
 
-    public void move(Location currentLocation, Direction direction){
+    public void deadEnemy(){
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 16; j++)
+                if (arr[i][j] == 'M') {
+                    if (arr[i+1][j] == 'X' || arr[i-1][j] == 'X' || arr[i][j+1] == 'X' || arr[i][j-1] == 'X') {
+                        arr[i][j] = ' ';
+                    }
+                }
+    }
 
+    public boolean checkEnemy(){
+        //enemy nearby
+        int check = 0;
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 16; j++)
+                if (arr[i][j] == 'X'){
+                    if (arr[i+1][j] == 'M' || arr[i-1][j] == 'M' || arr[i][j+1] == 'M' || arr[i][j-1] == 'M'){
+                        check = 1;
+                    }
+                }
+        if (check == 1){
+            return false;
+        } else{
+            return true;
+        }
+    }
+
+    public void move(String input){
+        //moves using WASD and ENTER
+        moveloop:
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 16; j++)
+                if (arr[i][j] == 'X') {
+                    if (input.equals("w")) {
+                        if (arr[i - 1][j] == ' ') {
+                            arr[i][j] = ' ';
+                            arr[i - 1][j] = 'X';
+                            break;
+                        }
+                    }
+                    else if (input.equals("a")) {
+                        if (arr[i][j - 1] == ' ') {
+                            arr[i][j] = ' ';
+                            arr[i][j - 1] = 'X';
+                            break;
+                        }
+                    }
+                    else if (input.equals("d")) {
+                        if (arr[i][j + 1] == ' ') {
+                            arr[i][j] = ' ';
+                            arr[i][j + 1] = 'X';
+                            break;
+                        }
+                    }
+                    else if (input.equals("s")) {
+                        if (arr[i + 1][j] == ' ') {
+                            arr[i][j] = ' ';
+                            arr[i + 1][j] = 'X';
+                            System.out.println('w');
+                            break moveloop;
+                        }
+                    }
+                }
     }
 
 
 
 }
+
 
