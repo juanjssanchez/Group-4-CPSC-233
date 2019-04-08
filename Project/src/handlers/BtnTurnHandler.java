3b4be7f6bc888954d809ec1b2cd1400e31783464
juanjssanchez <47364524+juanjssanchez@users.noreply.github.com>
2019-04-08 10:13:08 -0600

    Update BtnTurnHandler.java

--- Project/src/handlers/BtnTurnHandler.java
+++ Project/src/handlers/BtnTurnHandler.java
@@ -13,13 +13,13 @@ public class BtnTurnHandler implements EventHandler<ActionEvent> {
     }
 
     @Override
-    public void handle(ActionEvent event){
+    public void handle(ActionEvent event){ // 
         Button source = (Button) event.getSource();
 
         if (source.getText().contains("Attack")){
             //player.attack();
-            System.out.println("Player attacks " + player.getHealth());
-            player.decreaseHealth();
+            System.out.println("Player attacks " + player.getHealth());  // currently only used for feedback
+            player.decreaseHealth();                                     // player's turn will eventually be implemented
         } else if (source.getText().contains("Dodge")){
             System.out.println("Player dodges " + player.getHealth());
 
