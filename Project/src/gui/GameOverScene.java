48769d2dc829e81decd9d443646dc958adc3accd
juanjssanchez <47364524+juanjssanchez@users.noreply.github.com>
2019-04-12 22:03:01 -0600

    Add files via upload

--- Project/src/gui/GameOverScene.java
+++ Project/src/gui/GameOverScene.java
@@ -7,8 +7,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
 public class GameOverScene extends BaseScene {
 
@@ -35,6 +35,11 @@ public class GameOverScene extends BaseScene {
         StackPane pane = new StackPane();
         pane.getChildren().add(box);
         pane.setAlignment(Pos.CENTER);
+
+        BackgroundImage myBI = new BackgroundImage(new Image("file:img/backgroundmain.gif", 600, 500, false, true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        pane.setBackground(new Background(myBI));
 
         //Attach event handlers to buttons//
         QuitBtnHandler qHandler = new QuitBtnHandler();
